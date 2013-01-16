df0b5b34107e819f833222d33cc48f105dadaa54
bgeorgebbx <bgeorge@boomboxinc.com>
2013-01-16 14:23:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+var port = process.env.PORT || 3000
+server.listen(port, function() {
+  console.log( "Express server listening on port " + port );
+});
+
+app.get('/', function( req, res ) {
+  res.sendfile( __dirname + "/index.html" );
+});
+app.get('/socket.io/socket.io.js', function( req, res ) {
+  res.sendfile( __dirname + "/node_modules/socket.io/lib/socket.io.js" );
+});
+
+
+io.sockets.on('connection', function( socket ) {
+  socket.on('crack', function(data) {
+    socket.emit('whip', { val: "KA CRACK" });
+  });
+});
