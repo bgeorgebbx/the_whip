36a0b14dbca7fb34fab11eb07f1410e01460f1c4
bgeorgebbx <bgeorge@boomboxinc.com>
2013-01-17 11:00:17 -0500

    styled and added audio

--- app.js
+++ app.js
@@ -18,9 +18,21 @@ app.get('/', function( req, res ) {
 app.get('/socket.io/socket.io.js', function( req, res ) {
   res.sendfile( __dirname + "/node_modules/socket.io/lib/socket.io.js" );
 });
+app.get('/style.css', function( req, res ) {
+  res.sendfile( __dirname + "/public/stylesheets/style.css" );
+});
+app.get('/jones.png', function( req, res ) {
+  res.sendfile( __dirname + "/public/images/jones.png" );
+});
+app.get('/whip.mp3', function( req, res ) {
+  res.sendfile( __dirname + "/public/media/whip.mp3" );
+});
+app.get('/whip.ogg', function( req, res ) {
+  res.sendfile( __dirname + "/public/media/whip.ogg" );
+});
 
 io.sockets.on('connection', function( socket ) {
-  socket.emit( 'init', { msg: "initiated" });
+  socket.emit( 'init', { msg: "Connected." });
   socket.on('crack', function(data) {
     socket.broadcast.emit('whip', { val: "KA CRACK" });
   });
